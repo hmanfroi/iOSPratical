c479a1348cf6924d9f25080c7117e0dcc112c51b
Henrique Manfroi da Silveira <henrique_silveira@sicredi.com.br>
2021-03-18 15:31:44 -0300

    Start Project

---
+++ iOSPratical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPratical
+//
+//  Created by Henrique Manfroi on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
