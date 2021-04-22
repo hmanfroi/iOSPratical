181fdb8c1e7693e3801b56c5f09abfc8aa50b146
Lucas Real <lucas_real@terceiros.sicredi.com.br>
2021-04-22 16:30:36 -0300

    Tela de adicionar tarefa

--- iOSPratical/AddTask/AddTaskViewController.swift
+++ iOSPratical/AddTask/AddTaskViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 final class AddTaskViewController: UIViewController {
 
     var viewModel: AddTaskViewModel
-    
-    
+    let titleLabel: UILabel = UILabel()
+    let taskTextField: UITextField = UITextField()
+    let taskButton: UIButton = UIButton()
+
     init(viewModel: AddTaskViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -23,9 +25,52 @@ final class AddTaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
+        view.backgroundColor = .white
+        setupTitleLabel()
+        setupTaskTextField()
+        setupTaskButton()
+    }
+    
+    private func setupTitleLabel() {
+        view.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 74).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20).isActive = true
+        
+        titleLabel.text = "Adicionar uma tarefa"
     }
 
-
+    private func setupTaskTextField() {
+        view.addSubview(taskTextField)
+        
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false
+        taskTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        taskTextField.placeholder = "Título da tarefa"
+        taskTextField.layer.borderWidth = 1
+        taskTextField.layer.borderColor = UIColor.gray.cgColor
+    }
+    
+    private func setupTaskButton() {
+        view.addSubview(taskButton)
+        
+        taskButton.translatesAutoresizingMaskIntoConstraints = false
+        taskButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20).isActive = true
+        taskButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        taskButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        taskButton.setTitle("Adicionar tarefa", for: .normal)
+        taskButton.backgroundColor = .green
+        taskButton.setTitleColor(.black, for: .normal)
+        taskButton.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+    }
+    
+    @objc private func addTask() {
+        print("adicionei")
+    }
 }
 
